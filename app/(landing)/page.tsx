db56ef79c2972f80ca093b7f148fc0560ac26b06
chobolevel <rodaka123@naver.com>
2024-02-04 22:40:55 +0900

    #02-04 Notion Landing Page(Heroes)

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,10 +1,12 @@
 import Heading from "./_components/Heading"
+import Heroes from "./_components/Heroes"
 
 const LandingPage = () => {
   return (
     <div className="min-h-full flex flex-col">
       <div className="flex flex-1 flex-col items-center justify-center text-center gap-y-8 px-6 pb-10 md:justify-start">
         <Heading />
+        <Heroes />
       </div>
     </div>
   )
