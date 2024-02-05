9e44d10b930bf1d8cbdf4e7b1d501dc29697e578
chobolevel <rodaka123@naver.com>
2024-02-05 23:41:55 +0900

    #13 사이드바 접고 펼치는 기능 구현

--- app/(service)/_components/Navigation.tsx
+++ app/(service)/_components/Navigation.tsx
@@ -3,7 +3,7 @@
 import { cn } from "@/lib/utils"
 import { ChevronsLeft, MenuIcon } from "lucide-react"
 import { usePathname } from "next/navigation"
-import { ElementRef, useRef, useState } from "react"
+import { ElementRef, useEffect, useRef, useState } from "react"
 import { useMediaQuery } from "usehooks-ts"
 
 const Navigation = () => {
@@ -14,10 +14,65 @@ const Navigation = () => {
   const navbarRef = useRef<ElementRef<"div">>(null)
   const [isResetting, setIsResetting] = useState(false)
   const [isCollapsed, setIsCollapsed] = useState(isMobile)
+  useEffect(() => {
+    if (isMobile) {
+      collapse()
+    } else {
+      resetWidth()
+    }
+  }, [isMobile])
+  useEffect(() => {
+    if (isMobile) {
+      collapse()
+    }
+  }, [pathname, isMobile])
+  const handleMouseDown = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    e.preventDefault()
+    e.stopPropagation()
+    isResizeingRef.current = true
+    document.addEventListener("mousemove", handleMouseMove)
+    document.addEventListener("mouseup", handleMouseUp)
+  }
+  const handleMouseMove = (e: MouseEvent) => {
+    if (!isResizeingRef.current) return
+    let newWidth = e.clientX
+    if (newWidth < 240) newWidth = 240
+    if (newWidth > 480) newWidth = 480
+    if (sidebarRef.current && navbarRef.current) {
+      sidebarRef.current.style.width = `${newWidth}px`
+      navbarRef.current.style.setProperty("left", `${newWidth}px`)
+      navbarRef.current.style.setProperty("width", `calc(100% - ${newWidth}px)`)
+    }
+  }
+  const handleMouseUp = () => {
+    isResizeingRef.current = false
+    document.removeEventListener("mousemove", handleMouseMove)
+    document.removeEventListener("mouseup", handleMouseUp)
+  }
+  const resetWidth = () => {
+    if (sidebarRef.current && navbarRef.current) {
+      setIsCollapsed(false)
+      setIsResetting(false)
+      sidebarRef.current.style.width = isMobile ? "100%" : "240px"
+      navbarRef.current.style.setProperty("width", isMobile ? "0" : "calc(100% - 240px")
+      navbarRef.current.style.setProperty("left", isMobile ? "100%" : "240px")
+      setTimeout(() => setIsResetting(false), 300)
+    }
+  }
+  const collapse = () => {
+    if (sidebarRef.current && navbarRef.current) {
+      setIsCollapsed(true)
+      setIsResetting(true)
+      sidebarRef.current.style.width = "0"
+      navbarRef.current.style.setProperty("width", "100%")
+      navbarRef.current.style.setProperty("left", "0")
+      setTimeout(() => setIsResetting(false), 300)
+    }
+  }
   return (
     <>
-      <aside ref={sidebarRef} className={cn("group h-full bg-secondary overflow-y-auto relative w-60 flex flex-col z-[99999]", isResetting && "transition-all ease-in-out duration-300", isMobile && "w-0")}>
-        <div role="button" className={cn("h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute top-3 right-2 opacity-0 group-hover:opacity-100", isMobile && "opacity-100")}>
+      <aside ref={sidebarRef} className={cn("group h-full bg-secondary overflow-y-auto relative w-60 flex flex-col z-[99999] transition-all ease-in-out duration-300", isMobile && "w-0")}>
+        <div onClick={collapse} role="button" className={cn("h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute top-3 right-2 opacity-0 group-hover:opacity-100", isMobile && "opacity-100")}>
           <ChevronsLeft className="w-6 h-6" />
         </div>
         <div>
@@ -26,11 +81,11 @@ const Navigation = () => {
         <div className="mt-4">
           <p>Documents</p>
         </div>
-        <div className="opacity-0 group-hover:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0" />
+        <div onMouseDown={handleMouseDown} onClick={resetWidth} className="opacity-0 group-hover:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0" />
       </aside>
-      <div ref={navbarRef} className={cn("absolute top-0 left-60 z-[99999] w-[calc(100% - 240px)]", isResetting && "transition-all ease-in-out", isMobile && "left-0 w-full")}>
+      <div ref={navbarRef} className={cn("absolute top-0 left-60 z-[99999] w-[calc(100% - 240px)] transition-all ease-in-out", isMobile && "left-0 w-full")}>
         <nav className="bg-transparent px-4 py-2 w-full">
-          {isCollapsed && <MenuIcon role="button" className="h-6 w-6 text-muted-foreground" />}
+          {isCollapsed && <MenuIcon onClick={resetWidth} role="button" className="h-6 w-6 text-muted-foreground transition-all duration-300" />}
         </nav>
       </div>
     </>
--- app/(service)/layout.tsx
+++ app/(service)/layout.tsx
@@ -19,8 +19,7 @@ const ServiceLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full flex dark:bg-[#1f1f1f]">
       <Navigation />
-      <main className="flex-1 h-full overflow-y-auto"></main>
-      {children}
+      <main className="flex-1 h-full overflow-y-auto">{children}</main>
     </div>
   )
 }
