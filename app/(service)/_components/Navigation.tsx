22e5ca36db7e001ce1538d260a35a5036d625ba2
chobolevel <rodaka123@naver.com>
2024-02-05 22:16:59 +0900

    #11 Notion Sidebar

---
+++ app/(service)/_components/Navigation.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+const Navigation = () => {
+  return (
+    <>
+      <aside className="group h-full bg-secondary overflow-y-auto relative w-60 flex flex-col z-[99999]">
+        <div>
+          <p>Action</p>
+        </div>
+        <div className="mt-4">
+          <p>Documents</p>
+        </div>
+        <div className="opacity-0 group-hover:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0" />
+      </aside>
+    </>
+  )
+}
+
+export default Navigation
---
+++ app/(service)/layout.tsx
@@ -0,0 +1,28 @@
+"use client"
+
+import { Spinner } from "@/components/spinner"
+import { useConvexAuth } from "convex/react"
+import { redirect } from "next/navigation"
+import Navigation from "./_components/Navigation"
+
+const ServiceLayout = ({ children }: { children: React.ReactNode }) => {
+  const { isAuthenticated, isLoading } = useConvexAuth()
+  if (isLoading) {
+    return (
+      <div className="flex items-center h-full justify-center">
+        <Spinner size="lg" />
+      </div>)
+  }
+  if (!isAuthenticated) {
+    return redirect("/")
+  }
+  return (
+    <div className="h-full flex dark:bg-[#1f1f1f]">
+      <Navigation />
+      <main className="flex-1 h-full overflow-y-auto"></main>
+      {children}
+    </div>
+  )
+}
+
+export default ServiceLayout
