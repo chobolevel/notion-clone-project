d8493705fb3b8d97f644a86be879f61ffbc18862
chobolevel <rodaka123@naver.com>
2024-02-05 21:56:59 +0900

    #10 네비게이션바 인증로직 적용

--- app/(landing)/_components/Navbar.tsx
+++ app/(landing)/_components/Navbar.tsx
@@ -4,8 +4,14 @@ import { useScrollTrack } from "@/hooks/use-scroll-track"
 import { cn } from "@/lib/utils"
 import Logo from "./Logo"
 import { ModeToggle } from "@/components/mode-toggle"
+import { useConvexAuth } from "convex/react"
+import { SignInButton, UserButton } from "@clerk/clerk-react"
+import { Button } from "@/components/ui/button"
+import { Spinner } from "@/components/spinner"
+import Link from "next/link"
 
 const Navbar = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth()
   const scrolled = useScrollTrack()
   return (
     <div className={cn(
@@ -13,6 +19,27 @@ const Navbar = () => {
       scrolled && "border-b shadow-sm")}>
       <Logo />
       <div className="w-full flex items-center justify-between gap-x-2 md:ml-auto md:justify-end">
+        {isLoading && <Spinner />}
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode="modal">
+              <Button variant="ghost" size="sm">
+                Log in
+              </Button>
+            </SignInButton>
+            <SignInButton mode="modal">
+              <Button size="sm">Get Notion free</Button>
+            </SignInButton>
+          </>
+        )}
+        {isAuthenticated && !isLoading && (
+          <>
+            <Button variant="ghost" size="sm" asChild>
+              <Link href="/documents">Enter Notion</Link>
+            </Button>
+            <UserButton afterSignOutUrl="/" />
+          </>
+        )}
         <ModeToggle />
       </div>
     </div>
--- components/providers/convex-provider.tsx
+++ components/providers/convex-provider.tsx
@@ -1,18 +1,20 @@
+"use client"
 
-import "./index.css";
 import { ClerkProvider, useAuth } from "@clerk/clerk-react";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { ConvexReactClient } from "convex/react";
+import { ReactNode } from "react";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
-
-export const ConvexClientProvider = ({ children }: { children: React.ReactNode }) => {
+export const ConvexClientProvider = ({ children }: { children: ReactNode }) => {
   return (
-    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}>
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}
+    >
       <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
         {children}
       </ConvexProviderWithClerk>
     </ClerkProvider>
-  )
-}
+  );
+};
