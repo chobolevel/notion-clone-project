c85e986b8a2e583f2e2a8e487e3c1e04b6d0fc85
chobolevel <rodaka123@naver.com>
2024-02-06 10:49:02 +0900

    #17 Notion Document APIs 생성 - Document 생성

--- app/(service)/(routes)/documents/page.tsx
+++ app/(service)/(routes)/documents/page.tsx
@@ -1,6 +1,48 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import { useUser } from "@clerk/clerk-react"
+import { PlusCircle } from "lucide-react"
+import Image from "next/image"
+import { api } from "@/convex/_generated/api";
+import { useMutation } from "convex/react";
+import { toast } from "sonner";
+
 const DocumentPage = () => {
+  const { user } = useUser()
+  const create = useMutation(api.documents.create)
+  const handleCreate = () => {
+    const promise = create({ title: "test" })
+    toast.promise(promise, {
+      loading: "Create a new document...",
+      success: "New Document Created!",
+      error: "Failed to create a new document..."
+    })
+  }
   return (
-    <div>DocumentPage</div>
+    <div className="flex flex-col items-center justify-center h-full space-y-4">
+      <Image
+        src="/document.png"
+        width="300"
+        height="300"
+        alt="document-image"
+        className="dark:hidden"
+      />
+      <Image
+        src="/document-dark.png"
+        width="300"
+        height="300"
+        alt="document-image"
+        className="hidden dark:block"
+      />
+      <h2 className="">
+        Welcome to {user?.firstName}&apos;s Notion
+      </h2>
+      <Button onClick={handleCreate}>
+        <PlusCircle className="w-4 h-4 mr-2" />
+        Create a document
+      </Button>
+    </div>
   )
 }
 
